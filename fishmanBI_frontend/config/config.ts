b5c21971f02cf5d05190a92d906dbf39d596fb8a
fishman <cy13692810010@163.com>
2024-05-03 22:22:44 +0800

    开发登录注册页面

--- fishmanBI_frontend/config/config.ts
+++ fishmanBI_frontend/config/config.ts
@@ -138,8 +138,8 @@ export default defineConfig({
     {
       requestLibPath: "import { request } from '@umijs/max'",
       // 或者使用在线的版本
-      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
-      schemaPath: join(__dirname, 'oneapi.json'),
+      schemaPath: "http://localhost:8101/api/v2/api-docs",
+      projectName: 'fishmanBI',
       mock: false,
     },
     {
